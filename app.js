d794720001864ca1dbba0f767c18c449076574f0
Sascha Dietrich <sascha_dietrich@outlook.com>
2023-03-12 22:44:28 +0100

    feat: using handlebars

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  expressHbs.engine({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  /* res.status(404).sendFile(path.join(__dirname, "views", "404.html")); */
-  res.status(404).render("404", { docTitle: "Page not found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
-app.listen(2000);
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ const products = [];
 router.get("/add-product", (req, res, next) => {
   /* res.sendFile(path.join(rootDir, "views", "add-product.html")); */
   res.render("add-product", {
-    docTitle: "Add Product",
+    pageTitle: "Add Product",
     path: "/admin/add-product",
   });
 });
