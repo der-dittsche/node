8f1593ba67b8ff3b72b173caaf6d364a6dadb8a2
Sascha Dietrich <sascha_dietrich@outlook.com>
2023-03-13 02:27:55 +0100

    feat: using controller and model

--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,10 @@
 const express = require("express");
 
-/* const path = require("path");
-
-const rootDir = require("../util/path"); */
-
+const productControllers = require("../controllers/products");
 const router = express.Router();
 
-const products = [];
+router.get("/add-product", productControllers.getAddProduct);
 
-router.get("/add-product", (req, res, next) => {
-  /* res.sendFile(path.join(rootDir, "views", "add-product.html")); */
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-  });
-});
-
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+router.post("/add-product", productControllers.postProduct);
 
 exports.routes = router;
-exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,23 +1,9 @@
 const express = require("express");
-/* const path = require("path");
-
-const rootDir = require("../util/path"); */
 const adminData = require("./admin");
 
+const productControllers = require("../controllers/products");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  /*   console.log("shopProducts: ", adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html")); */
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+router.get("/", productControllers.getProducts);
 
 module.exports = router;
